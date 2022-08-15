2d2492ac933c4b2b83770b80f2f0bdefa9911690
sashacode11 <chan.sasha16@gmail.com>
2022-08-15 14:40:01 +0700

    bugfix issue #3

--- src/ts/main/quiz.ts
+++ src/ts/main/quiz.ts
@@ -90,7 +90,6 @@ export function continueButton(doc: Document) {
     if(elem !=null) {
         const rect = elem.getBoundingClientRect();
         const bottom = rect.bottom;
-        // continue_btn.style.position = "absolute";
         continue_btn.style.position = "relative";
         continue_btn.style.top = bottom + 15 + "px";
         continue_btn.style.right = "auto";
