b5335713090cb28b6c77a338c1a873c768161d3f
Sasha <chan.sasha16@gmail.com>
2022-08-21 17:56:04 +0700

    issue #16 hamburger icon

--- src/ts/main/main.ts
+++ src/ts/main/main.ts
@@ -1,6 +1,9 @@
 import {slides} from './quiz';
 import '../../../src/css/style1.css'
 sessionStorage.clear();
+
+import App from "../../../src/App.vue";
+
 //===========================================================================
 // un-comment for TESTING
 sessionStorage.setItem("random","false");
