d711051bfe17ad8ade3887efc0a62cb4ab561505
Sasha <chan.sasha16@gmail.com>
2022-08-22 11:03:53 +0700

    added first vue files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
+  <h1>{{ title }}</h1>
     <HamburgerIcon />
 </template>
 
 <script>
-    import HamburgerIcon from "../components/HamburgerIcon.vue";
+    import HamburgerIcon from "./components/HamburgerIcon.vue";
 export default {
   name: "App",
   components: { HamburgerIcon },
   data() {
     return {
-      
+      title: "You did it",
     };
   },
 };
--- src/ts/main/main.ts
+++ src/ts/main/main.ts
@@ -2,8 +2,6 @@ import {slides} from './quiz';
 import '../../../src/css/style1.css'
 sessionStorage.clear();
 
-import '../../vue/App.vue';
-
 //===========================================================================
 // un-comment for TESTING
 sessionStorage.setItem("random","false");
@@ -11,3 +9,18 @@ sessionStorage.setItem("random","false");
 // const file = './history.json';
 const file = '../../../src/courses/english.json';
 slides(file, document);
+
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "../../../src/App.vue";
+
+// import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
+
