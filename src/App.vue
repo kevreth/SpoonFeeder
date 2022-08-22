6c036a73b5dcffab7582621b1195ec61450080aa
Sasha <chan.sasha16@gmail.com>
2022-08-22 09:06:04 +0700

    added first vue files

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <HamburgerIcon />
+</template>
+
+<script>
+    import HamburgerIcon from "./components/HamburgerIcon.vue";
+export default {
+  name: "App",
+  components: { HamburgerIcon },
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/HamburgerIcon.vue
@@ -0,0 +1,18 @@
+<template>
+    <button>☰</button>
+</template>
+
+<script></script>
+
+<style scoped>
+button {
+  font-size: 5rem;
+  border: none;
+  padding: 5px 15px;
+  position: fixed;
+  top: 20px;
+  right: 25px;
+  transition: transform 0.3s ease-in-out;
+  width: 60px;
+}
+</style>
