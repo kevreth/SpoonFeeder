4e5f7f8a594d60cac59b74650ce2575783311ad3
kevreth <kevinrethwisch@gmail.com>
2022-08-22 00:36:04 -0400

    Vue integration

--- src/ts/main/main.ts
+++ src/ts/main/main.ts
@@ -14,8 +14,8 @@ slides(file, document);
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import App from "../../../src/App.vue";
-import Hamburger from "../../components/Hamburger.vue";
+import App from "../../App.vue";
+import Hamburger from "../../components/HamburgerIcon.vue";
 
 // import "./assets/main.css";
 
