8fb9889a4cc3d68336677a2aa4797ac0f5a5bb99
Sasha <chan.sasha16@gmail.com>
2022-08-22 14:40:48 +0700

    first added vue files

--- src/ts/main/main.ts
+++ src/ts/main/main.ts
@@ -11,16 +11,8 @@ const file = '../../../src/courses/english.json';
 slides(file, document);
 
 import { createApp } from "vue";
-import { createPinia } from "pinia";
-
 import App from "../../../src/App.vue";
-
-// import "./assets/main.css";
 
 const app = createApp(App);
 
-app.use(createPinia());
-app.use(router);
-
 app.mount("#app");
-
