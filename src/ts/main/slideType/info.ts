97cfea513e721e5aebc34f6a1a432751338e30c7
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-08-24 03:59:16 -0400

    18-courses-directories

--- src/ts/main/slideType/info.ts
+++ src/ts/main/slideType/info.ts
@@ -1,9 +1,10 @@
 import {Evaluation} from '../evaluation';
 import { Slide } from '../slide';
 import type {info} from '../course';
-import {showButton} from '../quiz';
+import {showSlides,continueButton} from '../quiz';
 import type { ResultReturnType } from '../result';
 import { SaveData } from '../saveData';
+import {makeButton} from "../utilities";
 export class Info extends Slide<string> {
     txt="";
     subtype="";
@@ -15,7 +16,7 @@ export class Info extends Slide<string> {
         const html = `\n${this.txt}`;
         this.createPageContent(html,doc);
         this.saveData();
-        showButton(doc);
+        this.showButton(doc);
     }
     evaluate(): Evaluation {
         return new Evaluation(0, 0, "");
@@ -26,5 +27,16 @@ export class Info extends Slide<string> {
     //don't record CCQ results
     getSaveData():SaveData {
         return new SaveData(this.txt,this.subtype, null);
+    }
+    //info has it's own showButton because the continue button
+    //appears immediately upon page load and can be in the wrong
+    //place because dynamic content in course files may cause it 
+    //to overlap. It gets placed before the dynamic content loads.
+    showButton(doc:Document):void {
+        const button = makeButton("btn", "continue-button", "continue");
+        const content = doc.getElementById('content') as HTMLElement;
+        content.insertAdjacentHTML('beforeend', '<br>'+ button);
+        const continue_btn = doc.getElementById("btn") as HTMLElement;
+        continue_btn?.addEventListener('click', ():void => {showSlides(doc)});
     }
 }
