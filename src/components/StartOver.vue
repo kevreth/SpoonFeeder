5d77661b90f8f73a3832d20cf601739b3a8b8243
Sasha <chan.sasha16@gmail.com>
2022-09-06 12:03:15 +0700

    ui tweaks #45

--- src/components/StartOver.vue
+++ src/components/StartOver.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="overlay">
-    <button @click="startOver" class="startOver">Start Over</button>
-    <a href="#" @click="closeOverlay">X</a>
+  <div class="startOver">
+    <button @click="startOver" class="startOverBtn">Start Over</button>
   </div>
 </template>
 
 <script>
 import startOver from "../composables/startOver";
+import overlay from "../css/overlay.css"
 
 export default {
+  data() {
+    return {
+      showOverlay: false
+    }
+  },
   methods: {
-    closeOverlay() {
-      this.$emit("close");
-    },
     startOver() {
       const { reloadPage } = startOver();
       return { reloadPage }
@@ -21,24 +23,4 @@ export default {
 };
 </script>
 
-<style>  
-  .overlay {
-        width: 200px;
-        padding: 25px;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        position: relative;
-        border-radius: 10px;
-    }
-  .startOver {
-        cursor: pointer;
-    }
-  a {
-        text-decoration: none;
-        position: absolute;
-        right: 0;
-        top: 0;
-        color: rgb(238, 123, 123);
-    }
-</style>
+<style></style>
