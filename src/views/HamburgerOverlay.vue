bc1b36e35bcd2f0ad3d302d7e31c56c836ad356f
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-06 01:34:58 -0400

    mostly works; need to change scope of overlay

--- src/views/HamburgerOverlay.vue
+++ src/views/HamburgerOverlay.vue
@@ -8,7 +8,6 @@
 
 <script>
 import StartOver from "../components/StartOver.vue"
-import overlay from "../css/overlay.css"
 
 export default {
     name: "HamburgerOverlay",
@@ -20,4 +19,36 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.overlay {
+  position: fixed;
+  width: 70%;
+  height: 70%;
+  top: 17%;
+  right: 15%;
+  margin: 5px;
+  background: #3a3a3a;
+  opacity: 0.5;
+  border-radius: 20px;
+  box-shadow: 2px 5px 10px rgba(241, 225, 225, 0.3);
+}
+.blury {
+  filter: blur(10px);
+}
+a {
+  text-decoration: none;
+}
+.closeOverlay {
+  color: rgb(253, 78, 78);
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 5px 8px;
+  margin: 5px 10px 0 0;
+  transition: 0.3s;
+  font-size: 56px;
+}
+.overlay a:hover, .overlay a:focus {
+  color: red;
+}
+</style>
