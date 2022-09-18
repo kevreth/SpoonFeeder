aa5bed0d6ce2a9f9419c3570f760ff6784347500
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-18 16:33:50 -0400

    refactor dead code

--- src/ts/main/slide.ts
+++ src/ts/main/slide.ts
@@ -1,7 +1,7 @@
 import type { Evaluation } from './evaluation';
 import type { SlideType } from './course';
 import type { ResultReturnType, AnswerType } from './result';
-import { extend, append, empty, getSavedDataArray } from './utilities';
+import { append, empty, getSavedDataArray } from './utilities';
 import { SaveData } from './saveData';
 import { mathjax } from 'mathjax-full/ts/mathjax';
 import { TeX } from 'mathjax-full/ts/input/tex';
