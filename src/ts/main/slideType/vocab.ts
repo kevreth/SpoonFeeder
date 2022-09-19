1733a43cb21c2e2bf79e52c05a54fce83ff454e0
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-19 07:44:29 -0400

    refactoring

--- src/ts/main/slideType/vocab.ts
+++ src/ts/main/slideType/vocab.ts
@@ -5,7 +5,6 @@ import { continueButton, showButton } from '../quiz/makeSlides';
 import { makeRow } from '../quiz/evaluate';
 import { removeListener, isRandom, shuffle, shuffleMap } from '../utilities';
 import { Result } from '../result';
-import { ResponseB } from '../response';
 import { Mc } from './mc';
 export type vocabTuplesType = [
   txt: string,
@@ -127,5 +126,47 @@ export class Vocab extends Slide<Array<string>> {
     const row_accum = rows.join('\n');
     const correctCtr = (this.result(ans, this.res) as Array<boolean>).filter(Boolean).length;
     return new Evaluation(this.list.size, correctCtr, row_accum);
+  }
+}
+class ResponseA {
+  txt:string;
+  ans:string;
+  res:string;
+  constructor(txt:string, ans:string, res:string) {
+      this.txt=txt;
+      this.ans=ans;
+      this.res=res;
+  }
+}
+class ResponseB {
+  resp:Array<ResponseA>=new Array<ResponseA>();
+  init(txt:Array<string>,ans:Array<string>, res:Array<string>) {
+      txt.forEach((txt1,idx) => {
+          const ans1=ans[idx];
+          const res1=res[idx];
+          this.resp.push(new ResponseA(txt1,ans1,res1));
+      })
+  }
+  [Symbol.iterator]():IterableIterator<ResponseA> {
+      return this.resp.values();
+  }
+  push(txt:string, ans:string, res:string):void {
+      const resp = new ResponseA(txt,ans,res);
+      this.resp.push(resp);
+  }
+  getAns():Array<string> {
+      return this.resp.map(a => a.ans);
+  }
+  getTxt():Array<string> {
+      return this.resp.map(a => a.txt);
+  }
+  getRes():Array<string> {
+      return this.resp.map(a => a.res);
+  }
+  itor() {
+      return this.resp.entries();
+  }
+  get():Array<ResponseA>{
+      return this.resp;
   }
 }
