71e73c4785bad0e2e711ff5b0d4c4405cb4802e0
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-24 10:20:34 -0400

    refactoring

--- src/ts/main/quiz/slide/setWidths.ts
+++ src/ts/main/quiz/slide/setWidths.ts
@@ -11,9 +11,9 @@ export class SetWidths {
     setWidths(ids, doc, maxWidth);
   }
   //sets the width on a different set of elements than whose width was measured
-  static readonly TARGETED: SetWidthTypeComplex = function(num: number, str1: string, str2: string, doc: Document) {
-    const fills: Array<string> = getIdsAsArray(num, str1);
-    const elements: Array<HTMLElement> = getNumberedElementsAsList(fills, doc);
+  static readonly TARGETED: SetWidthTypeComplex = function(num: number, str: string, str2: string, doc: Document) {
+    const ids: Array<string> = getIdsAsArray(num, str);
+    const elements: Array<HTMLElement> = getNumberedElementsAsList(ids, doc);
     const maxWidth = getMaxWidth(elements);
     const gaps: Array<string> = getIdsAsArray(num, str2);
     setWidths(gaps, doc, maxWidth);
