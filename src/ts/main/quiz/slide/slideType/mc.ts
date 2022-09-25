d2d4c4f2ce22a24065db349eb44b640fcb444a8d
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-25 15:55:28 -0400

    refactoring

--- src/ts/main/quiz/slide/slideType/mc.ts
+++ src/ts/main/quiz/slide/slideType/mc.ts
@@ -25,13 +25,14 @@ export class Mc extends Slide<string> {
     this.ans = this.o[0];
   }
   makeSlides(doc: Document): void {
+    const saveData = this.getSaveDataFunction();
     const shuffleFlag = this.isExercise && isRandom();
     let options = this.o;
     if (shuffleFlag) options = shuffle(options);
     const html = this.createHtml(this.txt, options);
     this.createPageContent(html, doc);
     options.forEach((option, optionCtr) => {
-      this.addBehavior(doc, option, options.length, optionCtr);
+      this.addBehavior(doc, option, options.length, optionCtr, saveData);
     });
     this.maxWidthStrategy(options.length,'btn', doc);
   }
@@ -39,7 +40,8 @@ export class Mc extends Slide<string> {
     doc: Document,
     option: string,
     length: number,
-    optionCtr: number
+    optionCtr: number,
+    saveData: any
   ): void {
     const element = doc.getElementById('btn' + optionCtr) as HTMLElement;
     element.addEventListener('click', () => {
@@ -50,7 +52,7 @@ export class Mc extends Slide<string> {
       this.res = option;
       if (this.result()) color = 'green';
       optionButton.style.backgroundColor = color;
-      this.saveData();
+      saveData();
       showButton(doc);
     });
   }
