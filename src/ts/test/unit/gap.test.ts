8e147d55efa851b1b6e1eec6f88537fad4ae6e43
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-25 11:45:49 -0400

    updated tests

--- src/ts/test/unit/gap.test.ts
+++ src/ts/test/unit/gap.test.ts
@@ -1,30 +1,30 @@
 import { expect, it } from 'vitest'
 import {Gap} from '../../main/quiz/slide/slideType/gap';
-// import "html-validate/jest";
-const question = "text (1) text (2) text (3)";
-const ans=["ans1", "ans2", "ans3"]
+// import 'html-validate/jest';
+const question = 'text (1) text (2) text (3)';
+const ans=['ans1', 'ans2', 'ans3']
 it('fills', () => {
 	const result = new Gap().fills(ans);
 	expect(result).not.toBeNull();
 	// expect(result).toHTMLValidate();
-	expect(result).toContain("fill0");
-	expect(result).toContain("fill2");
+	expect(result).toContain('fill0');
+	expect(result).toContain('fill2');
 });
 it('gaps', () => {
 	const result = new Gap().gaps(ans.length,question);
 	expect(result).not.toBeNull();
 	// expect(result).toHTMLValidate();
-	expect(result).toContain("(1) text");
-	expect(result).toContain("(3)");
+	expect(result).toContain('(1) text');
+	expect(result).toContain('(3)');
 });
 it('createHtml', () => {
 	const result = new Gap().createHtml(ans, question);
 	expect(result).not.toBeNull();
 	// expect(result).toHTMLValidate();
-	expect(result).toContain("fill0");
-	expect(result).toContain("fill2");
-	expect(result).toContain("ans1");
-	expect(result).toContain("ans3");
-	expect(result).toContain("(1) text");
-	expect(result).toContain("(3)");
+	expect(result).toContain('fill0');
+	expect(result).toContain('fill2');
+	expect(result).toContain('ans1');
+	expect(result).toContain('ans3');
+	expect(result).toContain('(1) text');
+	expect(result).toContain('(3)');
 });
