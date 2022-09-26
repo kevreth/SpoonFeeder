003e66cf8f8adc758459c41bfbb241339c40e21c
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-26 04:35:37 -0400

    refactoring

--- src/ts/main/quiz/slide/slideType/imap.ts
+++ src/ts/main/quiz/slide/slideType/imap.ts
@@ -4,7 +4,7 @@ import { showButton } from '../../makeSlides';
 import { SVGInjector } from '@tanem/svg-injector';
 import { getChildIds, removeListener } from '../../../utilities';
 import { Result } from '../strategies/result';
-import { CreateHtml } from '../strategies/createHtml';
+import { CreateHtml, ImapType } from '../strategies/createHtml';
 import { Evaluate } from '../strategies/evaluate';
 export class Imap extends Slide<string> {
   constructor() {
@@ -27,13 +27,8 @@ export class Imap extends Slide<string> {
     const txt = this.txt;
     const img = this.img;
     const createHtml = this.createHtml;
-    const html = createHtml(txt, img);
-    setValues.createPageContent(html, doc);
-    const picture = doc.getElementById('imagemap');
-    //inject SVG into page so it is part of DOM
-    SVGInjector(picture,  {afterAll() {afterAll(setValues,doc);}});
+    makeSlides2(createHtml, txt, img, setValues, doc);
   }
-
   public evaluate(): Evaluation {
     const txt = this.txt;
     const res = this.res;
@@ -42,7 +37,14 @@ export class Imap extends Slide<string> {
     return this.evaluateStrategy(txt, res, ans, result);
   }
 }
-function afterAll(imap: SetValues<string>, doc: Document) {
+function makeSlides2(createHtml: ImapType, txt: string, img: string, setValues: SetValues<string>, doc: Document) {
+  const html = createHtml(txt, img);
+  setValues.createPageContent(html, doc);
+  const picture = doc.getElementById('imagemap');
+  //inject SVG into page so it is part of DOM
+  SVGInjector(picture, { afterAll() { afterAll(setValues, doc); } });
+}
+function afterAll(setValues: SetValues<string>, doc: Document) {
   const ids = getChildIds(doc, 'imagemap');
   ids.forEach((id) => {
     const element = doc.getElementById(id) as HTMLElement;
@@ -52,12 +54,12 @@ function afterAll(imap: SetValues<string>, doc: Document) {
         element.classList.remove('shape');
         removeListener(element);
       });
-      imap.setRes(id);
+      setValues.setRes(id);
       const element = doc.getElementById(id) as HTMLElement;
       let classname = 'shape_incorrect';
-      if (imap.result()) classname = 'shape_correct';
+      if (setValues.result()) classname = 'shape_correct';
       element.classList.add(classname);
-      imap.saveData();
+      setValues.saveData();
       showButton(doc);
     });
   });
