fbb9315311f891863d566d25c06180db13d9f375
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-26 04:34:13 -0400

    refactoring

--- src/ts/main/quiz/slide/slideType/imap.ts
+++ src/ts/main/quiz/slide/slideType/imap.ts
@@ -1,5 +1,5 @@
 import { Evaluation } from '../../evaluate';
-import { Slide } from '../../slide';
+import { SetValues, Slide } from '../../slide';
 import { showButton } from '../../makeSlides';
 import { SVGInjector } from '@tanem/svg-injector';
 import { getChildIds, removeListener } from '../../../utilities';
@@ -23,16 +23,15 @@ export class Imap extends Slide<string> {
     } = json);
   }
   makeSlides(doc: Document): void {
-    /* eslint-disable  @typescript-eslint/no-this-alias */
-    const imap = this;
-    /* eslint-enable  @typescript-eslint/no-this-alias */
+    const setValues = this.getSetValues();
     const txt = this.txt;
     const img = this.img;
-    const html = this.createHtml(txt, img);
-    this.createPageContent(html, doc);
+    const createHtml = this.createHtml;
+    const html = createHtml(txt, img);
+    setValues.createPageContent(html, doc);
     const picture = doc.getElementById('imagemap');
     //inject SVG into page so it is part of DOM
-    SVGInjector(picture,  {afterAll() {afterAll(imap,doc);}});
+    SVGInjector(picture,  {afterAll() {afterAll(setValues,doc);}});
   }
 
   public evaluate(): Evaluation {
@@ -43,7 +42,7 @@ export class Imap extends Slide<string> {
     return this.evaluateStrategy(txt, res, ans, result);
   }
 }
-function afterAll(imap: Imap, doc: Document) {
+function afterAll(imap: SetValues<string>, doc: Document) {
   const ids = getChildIds(doc, 'imagemap');
   ids.forEach((id) => {
     const element = doc.getElementById(id) as HTMLElement;
@@ -53,9 +52,8 @@ function afterAll(imap: Imap, doc: Document) {
         element.classList.remove('shape');
         removeListener(element);
       });
-      imap.res = id;
-      const response = imap.res;
-      const element = doc.getElementById(response) as HTMLElement;
+      imap.setRes(id);
+      const element = doc.getElementById(id) as HTMLElement;
       let classname = 'shape_incorrect';
       if (imap.result()) classname = 'shape_correct';
       element.classList.add(classname);
