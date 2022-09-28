5e1d8bda1f84b83d2c48bdc046da9f5d4f957181
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-28 06:14:59 -0400

    refactoring

--- src/ts/main/quiz/slide/slideType/info.ts
+++ src/ts/main/quiz/slide/slideType/info.ts
@@ -1,15 +1,10 @@
 import { Slide, SlideInterface} from '../../slide';
-import { EvaluateType } from '../strategies/evaluate';
-import { CreateHtmlTypeInfo, CreateHtmlTypeIntersection } from '../strategies/createHtml';
-import { MakeSlidesInfoType, MakeSlidesType } from '../strategies/makeSlides';
-import { ResultType } from '../strategies/result';
+import { CreateHtmlTypeInfo } from '../strategies/createHtml';
+import { MakeSlidesInfoType } from '../strategies/makeSlides';
 export interface info extends SlideInterface {
   txt: string;
 }
 export class Info extends Slide<string> {
-  constructor(type:string, createHtml: CreateHtmlTypeIntersection, makeSlidesStrategy:MakeSlidesType, evaluateStrategy:EvaluateType, result: ResultType) {
-    super(type, createHtml, makeSlidesStrategy, evaluateStrategy, result);
-  }
   txt = '';
   processJson(json: Info): void {
     this.txt = json.txt;
