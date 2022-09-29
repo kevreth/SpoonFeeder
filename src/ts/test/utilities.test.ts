0a1745f21ffdb6da767d40e635a055b881affb05
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-29 05:57:52 -0400

    refactoring

---
+++ src/ts/test/utilities.test.ts
@@ -0,0 +1,49 @@
+import { expect, it } from 'vitest'
+import {isEqual, difference, intersection, makeButton, getChildIds}
+from '../main/utilities';
+import { JSDOM } from 'jsdom';
+const testArr1 = [1,2];
+const testArr2 = [2,3];
+it('makeButton', () => {
+  const str = makeButton('ABC', 'DEF', 'HIJ');
+  expect(str).toContain('id="ABC"');
+  expect(str).toContain('class="DEF"')
+  expect(str).toContain('>HIJ<')
+  expect(str).toBeDefined()
+  expect(str).not.to.be.empty
+  expect(str).to.be.string;
+});
+it('difference', () => {
+  const result = difference(testArr1,testArr2);
+  expect(result).toEqual([1]);
+});
+it('intersection', () => {
+  const result = intersection(testArr1,testArr2);
+  expect(result).toEqual([2]);
+});
+it('isEqual0', () => {
+  const result = isEqual(testArr1,testArr2);
+  expect(result).toEqual(false);
+});
+it('isEqual1', () => {
+  const result = isEqual(testArr1,testArr1);
+  expect(result).toEqual(true);
+});
+it('getChildIds',() => {
+  const dom = new JSDOM('<!DOCTYPE html><body></body>');
+  const html = `
+  <div id="parent-div">
+    <div id="div-no-1"></div>
+    <div id="div-no-2"></div>
+    <div id="div-no-3"></div>
+    <div id="div-no-4"></div>
+  </div>
+  `;
+  dom.window.document.body.innerHTML = html;
+  const doc = dom.window.document;
+  const ids = getChildIds(doc, 'parent-div');
+  expect(ids).not.toBeNull()
+  expect(ids).not.to.be.empty;
+  expect(ids.length).toBe(4);
+  expect(ids[2]).toBe('div-no-3');
+})
