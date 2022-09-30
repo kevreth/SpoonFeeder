ae544ea94fb92c8f1d576634af410bc006692faa
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-30 19:19:27 -0400

    creating unit tests

--- src/ts/main/quiz/SlideInterface.ts
+++ src/ts/main/quiz/SlideInterface.ts
@@ -1,11 +1,22 @@
 import type { Evaluation } from './evaluate';
-import type { AnswerType, ResultReturnType } from './slide/strategies/result';
+import type { CreateHtmlTypeIntersection } from './slide/strategies/createHtml';
+import type { EvaluateType } from './slide/strategies/evaluate';
+import type { MakeSlidesType } from './slide/strategies/makeSlides';
+import type {
+  AnswerType,
+  ResultReturnType,
+  ResultType,
+} from './slide/strategies/result';
 
 export interface SlideInterface {
   txt: AnswerType;
   type: string;
   isExercise: boolean;
   pageTemplate: string;
+  createHtml: CreateHtmlTypeIntersection;
+  makeSlidesStrategy: MakeSlidesType;
+  evaluateStrategy: EvaluateType;
+  resultType: ResultType;
   //Transform human-created YML into computer-friendly JSON
   //Run before quiz starts
   processJson(json: SlideInterface): void;
--- src/ts/test/quiz/processJson.test.ts
+++ src/ts/test/quiz/processJson.test.ts
@@ -1,4 +1,54 @@
-import { it } from 'vitest';
-it('createHtml', () => {
-  return;
+import { expect, it } from 'vitest';
+import { ProcessJson } from '../../main/quiz/processJson';
+import { Mc } from '../../main/quiz/slide/slideType/mc';
+const testjson = `
+  {
+    "name": "course 1",
+    "units": [
+      {
+        "name": "unit 1",
+        "lessons": [
+          {
+            "name": "lesson 1",
+            "modules": [
+              {
+                "name": "module 1",
+                "inst": [
+                  {
+                    "type": "info",
+                    "txt": "info1"
+                  }
+                ],
+                "exercises": [
+                  {
+                    "type": "mc",
+                    "txt": "mc",
+                    "o": ["o1", "o2"]
+                  },
+                  {
+                    "type": "gap",
+                    "text": "text (1) text (2)",
+                    "ans": ["ans1", "ans2"]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+`;
+sessionStorage.setItem('random', 'false');
+it('testProcessSlides', () => {
+  const slideArr = ProcessJson.processJson(JSON.parse(testjson));
+  expect(slideArr).toBeDefined();
+  expect(slideArr).not.to.be.empty;
+  expect(slideArr.length).toEqual(7);
+  const slide = slideArr[5];
+  expect(slide).toBeDefined();
+  expect(slide).not.to.be.empty;
+  expect(slide).toBeInstanceOf(Mc);
+  expect((slide as Mc).o[0]).toBe('o1');
+  expect((slide as Mc).txt).toBe('mc');
 });
