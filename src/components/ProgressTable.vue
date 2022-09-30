52f3bbe86ba24bb4767445c79ebca3ec688637ef
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-30 05:59:50 -0400

    externalized QHierarchy data and columns into a composable

--- src/components/ProgressTable.vue
+++ src/components/ProgressTable.vue
@@ -13,101 +13,10 @@
 
 <script setup>
 import { ref } from 'vue';
+import {Progress} from '../composables/progress';
 
-const columns = ref([
-  {
-    name: 'label',
-    label: 'Label',
-    align: 'left',
-    field: 'label',
-    // (optional) tell QHierarchy you want this column sortable
-    sortable: true,
-  },
-  {
-    name: 'Description',
-    label: 'Description',
-    sortable: true,
-    field: 'description',
-    align: 'center',
-  },
-  {
-    name: 'note',
-    label: 'Note',
-    sortable: true,
-    field: 'note',
-    align: 'left',
-  },
-]);
-const data = ref([
-  {
-    label: 'Node 1',
-    description: 'Node 1 description data',
-    note: 'Node 1 note',
-    // id: 1,
-    children: [
-      {
-        label: 'Node 1.1',
-        description: 'Node 1.1 description',
-        note: 'Node 1.1 note',
-        // id: 2
-      },
-      {
-        label: 'Node 1.2',
-        description: 'Node 1.2 description',
-        note: 'Node 1.2 note',
-        // id: 3,
-        children: [
-          {
-            label: 'Node 1.2.1',
-            description: 'Node 1.2.1 description',
-            note: 'Node 1.2.1 note',
-            // id: 4
-          },
-          {
-            label: 'Node 1.2.2',
-            description: 'Node 1.2.2 description',
-            note: 'Node 1.2.2 note',
-            // id: 5
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: 'Node 2',
-    description: 'Node 2 description',
-    note: 'Node 2 note',
-    // id: 6,
-    children: [
-      {
-        label: 'Node 2.1',
-        description: 'Node 2.1 description',
-        note: 'Node 2.1 note',
-        // id: 7,
-        children: [
-          {
-            label: 'Node 2.1.1',
-            description: 'Node 2.1.1 description',
-            note: 'Node 2.1.1 note',
-            // id: 8
-          },
-          {
-            label: 'Node 2.1.2',
-            description: 'Node 2.1.2 description',
-            note: 'Node 2.1.2 note',
-            // id: 9
-          },
-        ],
-      },
-      {
-        label: 'Node 2.2',
-        description: 'Node 2.2 description',
-        note: 'Node 2.2 note',
-        // id: 10
-      },
-    ],
-  },
-]);
+const columns = ref(Progress.columns);
+const data = ref(Progress.data);
 
 const classes = ref('bg-secondary');
 const dark = ref(true);
