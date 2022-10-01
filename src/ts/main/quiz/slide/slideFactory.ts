8323b54684155a822654928c0539702bd9b37609
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-09-30 20:11:30 -0400

    creating unit tests

--- src/ts/main/quiz/slide/slideFactory.ts
+++ src/ts/main/quiz/slide/slideFactory.ts
@@ -13,34 +13,17 @@ import { Evaluate } from './strategies/evaluate';
 import { MakeSlides } from './strategies/makeSlides';
 import { Result } from './strategies/result';
 export class SlideFactory {
-  public static readonly BOOL = new SlideFactory('bool');
-  public static readonly GAP = new SlideFactory('gap');
-  public static readonly IMAP = new SlideFactory('imap');
-  public static readonly INFO = new SlideFactory('info');
-  public static readonly MC = new SlideFactory('mc');
-  public static readonly SELECT = new SlideFactory('select');
-  public static readonly SORT = new SlideFactory('sort');
-  public static readonly VOCAB = new SlideFactory('vocab');
-  public static get values(): SlideFactory[] {
-    return [
-      this.BOOL,
-      this.GAP,
-      this.IMAP,
-      this.INFO,
-      this.MC,
-      this.SELECT,
-      this.SORT,
-      this.VOCAB,
-    ];
+  private static get values(): string[] {
+    return ['bool', 'gap', 'imap', 'info', 'mc', 'select', 'sort', 'vocab'];
   }
-  private constructor(public readonly name: string) {}
-  public static instance(instanceType: string): SlideInterface | undefined {
-    const numTypes = SlideFactory.values.length;
+  public static getInstance(instanceType: string): SlideInterface | undefined {
+    const values = SlideFactory.values;
+    const numTypes = values.length;
     for (let i = 0; i < numTypes; i++) {
-      const type = SlideFactory.values[i].name;
+      const type = values[i];
       if (type === instanceType) {
-        switch (SlideFactory.values[i]) {
-          case SlideFactory.BOOL:
+        switch (values[i]) {
+          case 'bool':
             return new Bool(
               type,
               CreateHtml.MC as CreateHtmlTypeIntersection,
@@ -48,7 +31,7 @@ export class SlideFactory {
               Evaluate.SIMPLE,
               Result.SIMPLE
             );
-          case SlideFactory.GAP:
+          case 'gap':
             return new Gap(
               type,
               CreateHtml.GAP as CreateHtmlTypeIntersection,
@@ -56,7 +39,7 @@ export class SlideFactory {
               Evaluate.GAP,
               Result.CORRELATED
             );
-          case SlideFactory.IMAP:
+          case 'imap':
             return new Imap(
               type,
               CreateHtml.IMAP as CreateHtmlTypeIntersection,
@@ -64,7 +47,7 @@ export class SlideFactory {
               Evaluate.SIMPLE,
               Result.SIMPLE
             );
-          case SlideFactory.INFO:
+          case 'info':
             return new Info(
               type,
               CreateHtml.INFO as CreateHtmlTypeIntersection,
@@ -72,7 +55,7 @@ export class SlideFactory {
               Evaluate.DEFAULT,
               Result.UNSUPPORTED
             );
-          case SlideFactory.MC:
+          case 'mc':
             return new Mc(
               type,
               CreateHtml.MC as CreateHtmlTypeIntersection,
@@ -80,7 +63,7 @@ export class SlideFactory {
               Evaluate.SIMPLE,
               Result.SIMPLE
             );
-          case SlideFactory.SELECT:
+          case 'select':
             return new Select(
               type,
               CreateHtml.SELECT as CreateHtmlTypeIntersection,
@@ -88,7 +71,7 @@ export class SlideFactory {
               Evaluate.SIMPLE,
               Result.LIST
             );
-          case SlideFactory.SORT:
+          case 'sort':
             return new Sort(
               type,
               CreateHtml.SORT as CreateHtmlTypeIntersection,
@@ -96,7 +79,7 @@ export class SlideFactory {
               Evaluate.SIMPLE,
               Result.LIST
             );
-          case SlideFactory.VOCAB:
+          case 'vocab':
             return new Vocab(
               type,
               CreateHtml.MC as CreateHtmlTypeIntersection,
@@ -107,8 +90,5 @@ export class SlideFactory {
         }
       }
     }
-  }
-  public static getInstance(instanceType: string): SlideInterface | undefined {
-    return SlideFactory.instance(instanceType);
   }
 }
--- src/ts/test/quiz/slide/slideFactory.test.ts
+++ src/ts/test/quiz/slide/slideFactory.test.ts
@@ -1,4 +1,7 @@
-import { it } from 'vitest';
-it('createHtml', () => {
-  return;
+import { expect, it } from 'vitest';
+import { SlideFactory } from '../../../main/quiz/slide/slideFactory';
+import { Mc } from '../../../main/quiz/slide/slideType/mc';
+it('testGetInstance', () => {
+  const slide = SlideFactory.getInstance('mc');
+  expect(slide).toBeInstanceOf(Mc);
 });
