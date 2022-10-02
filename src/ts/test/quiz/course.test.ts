a82ff4f0aaf022b2f87945c2fa0591f106306f2c
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-10-02 01:02:05 -0400

    refactoring

--- src/ts/test/quiz/course.test.ts
+++ src/ts/test/quiz/course.test.ts
@@ -1,4 +1,33 @@
 import { expect, it } from 'vitest';
-it('description', () => {
-  expect(0).toEqual(0);
+import { Course, Lesson, Module, Unit } from '../../main/quiz/course';
+it('test course', () => {
+  const division = new Course();
+  const children = division.units;
+  expect(division).not.toBeNull();
+  expect(children).not.toBeNull();
+  expect(children).toHaveLength(0);
+});
+it('test unit', () => {
+  const division = new Unit();
+  const children = division.lessons;
+  expect(division).not.toBeNull();
+  expect(children).not.toBeNull();
+  expect(children).toHaveLength(0);
+});
+it('test lesson', () => {
+  const division = new Lesson();
+  const children = division.modules;
+  expect(division).not.toBeNull();
+  expect(children).not.toBeNull();
+  expect(children).toHaveLength(0);
+});
+it('test module', () => {
+  const division = new Module();
+  const inst = division.inst;
+  const exercises = division.exercises;
+  expect(division).not.toBeNull();
+  expect(inst).not.toBeNull();
+  expect(exercises).not.toBeNull();
+  expect(inst).toHaveLength(0);
+  expect(exercises).toHaveLength(0);
 });
