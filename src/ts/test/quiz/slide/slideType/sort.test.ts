2ee97123404f11ee3dea789efe473c18a41c1d11
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-10-02 23:34:03 -0400

    refactoring

--- src/ts/test/quiz/slide/slideType/sort.test.ts
+++ src/ts/test/quiz/slide/slideType/sort.test.ts
@@ -1,22 +1,40 @@
-import { beforeEach, it } from 'vitest';
+import { beforeEach, expect, it, vi } from 'vitest';
 import { Slide } from '../../../../main/quiz/slide';
+import { Sort } from '../../../../main/quiz/slide/slideType/sort';
 import { SORT } from '../../../../main/quiz/slideFactory';
 import { SlideTest } from '../../slide.test';
 class Test extends SlideTest<string[]> {
+  type = 'sort';
   public processJson(): void {
-    throw new Error('Method not implemented.');
-  }
-  public makeSlides(): void {
-    throw new Error('Method not implemented.');
+    const param = SORT();
+    param.txt = 'no';
+    param.ans = ['no', 'yes'];
+    param.isExercise = true;
+    const testable = test.getTestable() as Sort;
+    testable.processJson(param);
+    expect(testable.type).toEqual(param.type);
+    expect(testable.txt).toEqual(param.txt);
+    expect(testable.ans).toEqual(param.ans);
+    expect(testable.isExercise).toEqual(param.isExercise);
   }
   protected factory(): Slide<string[]> {
-    return SORT();
+    const createHtml = vi.fn();
+    const makeSlides = vi.fn();
+    const evaluate = vi.fn();
+    const result = vi.fn();
+    return new Sort(this.type, createHtml, makeSlides, evaluate, result);
   }
 }
 const test = new Test();
 beforeEach(() => {
   test.beforeEach();
 });
+it('processJson', () => {
+  test.processJson();
+});
 it('getSetValues', () => {
   test.getSetValues();
+});
+it('getSetValues', () => {
+  test.makeSlides();
 });
