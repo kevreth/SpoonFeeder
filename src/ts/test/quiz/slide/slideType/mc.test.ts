f03e3156b81c253d02551748e7b6048f4e1a384b
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-10-02 23:04:05 -0400

    refactoring

--- src/ts/test/quiz/slide/slideType/mc.test.ts
+++ src/ts/test/quiz/slide/slideType/mc.test.ts
@@ -1,22 +1,41 @@
-import { beforeEach, it } from 'vitest';
+import { beforeEach, expect, it, vi } from 'vitest';
 import { Slide } from '../../../../main/quiz/slide';
+import { Mc } from '../../../../main/quiz/slide/slideType/mc';
 import { MC } from '../../../../main/quiz/slideFactory';
 import { SlideTest } from '../../slide.test';
 class Test extends SlideTest<string> {
+  type = 'mc';
   public processJson(): void {
-    throw new Error('Method not implemented.');
-  }
-  public makeSlides(): void {
-    throw new Error('Method not implemented.');
+    const param = MC();
+    param.txt = 'yes';
+    param.ans = 'yes';
+    param.o = ['yes', 'no'];
+    const testable = test.getTestable() as Mc;
+    testable.o = ['yes', 'no'];
+    testable.processJson(param);
+    expect(testable.type).toEqual(param.type);
+    expect(testable.txt).toEqual(param.txt);
+    expect(testable.ans).toEqual(param.ans);
+    expect(testable.o).toEqual(param.o);
   }
   protected factory(): Slide<string> {
-    return MC();
+    const createHtml = vi.fn();
+    const makeSlides = vi.fn();
+    const evaluate = vi.fn();
+    const result = vi.fn();
+    return new Mc(this.type, createHtml, makeSlides, evaluate, result);
   }
 }
 const test = new Test();
 beforeEach(() => {
   test.beforeEach();
 });
+it('processJson', () => {
+  test.processJson();
+});
 it('getSetValues', () => {
   test.getSetValues();
+});
+it('getSetValues', () => {
+  test.makeSlides();
 });
