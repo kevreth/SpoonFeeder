3085558b47389d4e3c0bfdad2e305a162b137d37
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-10-02 22:51:55 -0400

    refactoring

--- src/ts/test/quiz/slide/slideType/imap.test.ts
+++ src/ts/test/quiz/slide/slideType/imap.test.ts
@@ -1,22 +1,40 @@
-import { beforeEach, it } from 'vitest';
+import { beforeEach, expect, it, vi } from 'vitest';
 import { Slide } from '../../../../main/quiz/slide';
+import { Imap } from '../../../../main/quiz/slide/slideType/imap';
 import { IMAP } from '../../../../main/quiz/slideFactory';
 import { SlideTest } from '../../slide.test';
 class Test extends SlideTest<string> {
+  type = 'imap';
   public processJson(): void {
-    throw new Error('Method not implemented.');
-  }
-  public makeSlides(): void {
-    throw new Error('Method not implemented.');
+    const param = IMAP();
+    param.txt = 'no';
+    param.ans = 'no';
+    param.isExercise = true;
+    const testable = test.getTestable();
+    testable.processJson(param);
+    expect(testable.type).toEqual(param.type);
+    expect(testable.txt).toEqual(param.txt);
+    expect(testable.ans).toEqual(param.ans);
+    expect(testable.isExercise).toEqual(param.isExercise);
   }
   protected factory(): Slide<string> {
-    return IMAP();
+    const createHtml = vi.fn();
+    const makeSlides = vi.fn();
+    const evaluate = vi.fn();
+    const result = vi.fn();
+    return new Imap(this.type, createHtml, makeSlides, evaluate, result);
   }
 }
 const test = new Test();
 beforeEach(() => {
   test.beforeEach();
 });
+it('processJson', () => {
+  test.processJson();
+});
 it('getSetValues', () => {
   test.getSetValues();
+});
+it('getSetValues', () => {
+  test.makeSlides();
 });
