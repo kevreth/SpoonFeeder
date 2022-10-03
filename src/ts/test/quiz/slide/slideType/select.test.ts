8f266e16f74c7bc5a22cfe9afcdbb960e398c9ce
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-10-03 06:33:57 -0400

    refactoring

--- src/ts/test/quiz/slide/slideType/select.test.ts
+++ src/ts/test/quiz/slide/slideType/select.test.ts
@@ -1,22 +1,40 @@
-import { beforeEach, it } from 'vitest';
+import { beforeEach, expect, it, vi } from 'vitest';
 import { Slide } from '../../../../main/quiz/slide';
+import { Select } from '../../../../main/quiz/slide/slideType/select';
 import { SELECT } from '../../../../main/quiz/slideFactory';
 import { SlideTest } from '../../slide.test';
 class Test extends SlideTest<number[]> {
+  type = 'select';
   public processJson(): void {
-    throw new Error('Method not implemented.');
-  }
-  public makeSlides(): void {
-    throw new Error('Method not implemented.');
+    const param = SELECT();
+    param.txt = 'test this';
+    param.ans = [1];
+    param.isExercise = true;
+    const testable = test.getTestable() as Select;
+    testable.processJson(param);
+    expect(testable.type).toEqual(param.type);
+    expect(testable.txt).toEqual(param.txt);
+    expect(testable.ans).toEqual(param.ans);
+    expect(testable.isExercise).toEqual(param.isExercise);
   }
   protected factory(): Slide<number[]> {
-    return SELECT();
+    const createHtml = vi.fn();
+    const makeSlides = vi.fn();
+    const evaluate = vi.fn();
+    const result = vi.fn();
+    return new Select(this.type, createHtml, makeSlides, evaluate, result);
   }
 }
 const test = new Test();
 beforeEach(() => {
   test.beforeEach();
 });
+it('processJson', () => {
+  test.processJson();
+});
 it('getSetValues', () => {
   test.getSetValues();
+});
+it('getSetValues', () => {
+  test.makeSlides();
 });
