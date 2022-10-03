9d18f7b0b1a23d95d7fb5b62a8e10aefe78a8a2f
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-10-03 09:56:34 -0400

    refactoring

--- src/ts/test/quiz/evaluate.test.ts
+++ src/ts/test/quiz/evaluate.test.ts
@@ -1,5 +1,10 @@
 import { expect, it } from 'vitest';
 import { Evaluation } from '../../main/quiz/evaluate';
+it('makeRow', () => {
+  const actual = Evaluation.makeRow('q', 'a', 'c');
+  const expected = '<tr><td>q</td><td>%N%.</td><td>a</td><td>c</td></tr>';
+  expect(actual).toBe(expected);
+});
 it('percentCorrect', () => {
   const actual = Evaluation.percentCorrect(5, 8);
   expect(actual).toBe('63');
