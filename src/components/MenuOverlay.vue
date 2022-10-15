412ac10cfd24a01fc3e22b28c276b44b5b85a763
Sasha <chan.sasha16@gmail.com>
2022-10-15 09:30:21 +0700

    fixed esc close menu overlay

--- src/components/MenuOverlay.vue
+++ src/components/MenuOverlay.vue
@@ -1,5 +1,5 @@
 <template>
-  <MenuBtn @click="handleOverlay" @keydown="overlay = false"/>
+  <MenuBtn @click="overlay = true" @keydown.esc="overlay = false"/>
 
   <q-overlay v-model="overlay">
     <template #body>        
