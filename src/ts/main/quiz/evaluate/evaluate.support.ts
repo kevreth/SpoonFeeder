e22c17ff3cffc20034ac4c2b6a7fa60c77f1d435
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-10-16 10:55:07 -0400

    adding scoring functionality

--- src/ts/main/quiz/evaluate/evaluate.support.ts
+++ src/ts/main/quiz/evaluate/evaluate.support.ts
@@ -28,33 +28,36 @@ export function numbering(responseCtr: number, text: string) {
   }
   return text;
 }
-export function processSlide(slide: SlideInterface, evalAccum: Evaluation) {
-  const evaluation: Evaluation = slide.evaluate();
-  evalAccum.responses += evaluation.responses;
-  evalAccum.correct += evaluation.correct;
-  evalAccum.text = evalAccum.text.concat(evaluation.text);
-}
-export function evaluate(slides: SlideInterface[]): string {
-  const evalAccum = evalBody(slides);
+export function evaluate(slidesArr: SlideInterface[]): string {
+  const evals = getEvaluationArray(slidesArr);
+  const TABLE_HEADER =
+    '<table><tr><th>Question</th><th></th><th>Your answer</th><th>Correct Answer</th></tr>';
+  const evalAccum = new Evaluation(0, 0, TABLE_HEADER);
+  for (const evaluation of evals) {
+    evalAccum.responses += evaluation.responses;
+    evalAccum.correct += evaluation.correct;
+    evalAccum.text = evalAccum.text.concat(evaluation.text);
+  }
+  evalAccum.text = numbering(evalAccum.responses, evalAccum.text);
+  evalAccum.text = evalAccum.text.concat('</table>');
   const txt0 = evalAccum.text;
   const correct = evalAccum.correct;
   const responses = evalAccum.responses;
   const txt1 = evalStats(correct, responses);
   return txt0.concat(txt1);
 }
-export function evalBody(slidesArr: SlideInterface[]) {
-  const TABLE_HEADER =
-    '<table><tr><th>Question</th><th></th><th>Your answer</th><th>Correct Answer</th></tr>';
-  const evalAccum = new Evaluation(0, 0, TABLE_HEADER);
+export function getEvaluationArray(slidesArr: SlideInterface[]) {
   const slides = Queue.from(slidesArr);
   const size = slides.size;
+  const evals = new Array<Evaluation>();
   for (let i = 0; i < size; i++) {
     const slide = slides.dequeue();
-    if (slide && slide.isExercise) processSlide(slide, evalAccum);
+    if (slide && slide.isExercise) {
+      const evaluation: Evaluation = slide.evaluate();
+      evals.push(evaluation);
+    }
   }
-  evalAccum.text = numbering(evalAccum.responses, evalAccum.text);
-  evalAccum.text = evalAccum.text.concat('</table>');
-  return evalAccum;
+  return evals;
 }
 export function evalStats(correct: number, responses: number) {
   const pctCorrect = percentCorrect(correct, responses);
