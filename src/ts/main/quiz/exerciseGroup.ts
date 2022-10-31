b29aca93c68de8093de9ca4c559a0f1060780291
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-10-31 05:41:35 -0400

    refactoring

--- src/ts/main/quiz/exerciseGroup.ts
+++ src/ts/main/quiz/exerciseGroup.ts
@@ -30,3 +30,56 @@ export function exerciseGroupSlideIndex(
   }
   return retval;
 }
+export function exerciseGroupMakeSlides(
+  saves: SaveData[],
+  slide: SlideInterface,
+  idx: number,
+  doc: Document,
+  reloadSlide: (slide: SlideInterface, idx: number, doc: Document) => void
+) {
+  const results = Array<string>();
+  for (const saved of saves) {
+    const idx2 = (slide.txt as string[]).findIndex((x) =>
+      isEqual(x, saved.txt as string)
+    );
+    if (idx2 > -1) {
+      results.push(saved.result as string);
+    }
+  }
+  //TODO: should this just test on the length, or the content?
+  if (isEqual(results.length, slide.txt.length)) {
+    reloadSlide(slide, idx, doc);
+  }
+  //not all slide questions answered
+  else {
+    const results = Array<string>();
+    for (const saved of saves) {
+      const idx2 = (slide.txt as string[]).findIndex((x) =>
+        isEqual(x, saved.txt as string)
+      );
+      if (idx2 > -1) {
+        results.push(saved.result as string);
+      }
+    }
+    slide.setResults(results);
+    slide.makeSlides(doc);
+  }
+}
+export function exerciseGroupReloadSlide(
+  saves: SaveData[],
+  slide: SlideInterface,
+  doc: Document,
+  showSlides: (doc: Document) => void
+) {
+  const results = Array<string>();
+  for (const saved of saves) {
+    const idx2 = (slide.txt as string[]).findIndex((x) =>
+      isEqual(x, saved.txt as string)
+    );
+    if (idx2 > -1) {
+      results.push(saved.result as string);
+    }
+  }
+  slide.setResults(results);
+  showSlides(doc);
+}
--- src/ts/main/quiz/makeSlides.ts
+++ src/ts/main/quiz/makeSlides.ts
@@ -1,7 +1,11 @@
 import reloadPage from '../../../composables/startOver';
 import { Json } from '../globals';
-import { isEqual, makeButton } from '../utilities';
+import { makeButton } from '../utilities';
 import { evaluate } from './evaluate';
+import {
+  exerciseGroupMakeSlides,
+  exerciseGroupReloadSlide,
+} from './exerciseGroup';
 import { Slide } from './slide';
 import { SaveData } from './slide/saveData';
 import type { SlideInterface } from './slideInterface';
@@ -56,62 +60,6 @@ export class MakeSlides {
     startOverButton(doc);
   }
 }
-export function exerciseGroupMakeSlides(
-  saves: SaveData[],
-  slide: SlideInterface,
-  idx: number,
-  doc: Document,
-  reloadSlide: (slide: SlideInterface, idx: number, doc: Document) => void
-) {
-  const results = Array<string>();
-  for (const saved of saves) {
-    const idx2 = (slide.txt as string[]).findIndex((x) =>
-      isEqual(x, saved.txt as string)
-    );
-    if (idx2 > -1) {
-      results.push(saved.result as string);
-    }
-  }
-  //TODO: should this just test on the length, or the content?
-  if (isEqual(results.length, slide.txt.length)) {
-    reloadSlide(slide, idx, doc);
-  }
-
-  //not all slide questions answered
-  else {
-    const results = Array<string>();
-    for (const saved of saves) {
-      const idx2 = (slide.txt as string[]).findIndex((x) =>
-        isEqual(x, saved.txt as string)
-      );
-      if (idx2 > -1) {
-        results.push(saved.result as string);
-      }
-    }
-    slide.setResults(results);
-    slide.makeSlides(doc);
-  }
-}
-
-export function exerciseGroupReloadSlide(
-  saves: SaveData[],
-  slide: SlideInterface,
-  doc: Document,
-  showSlides: (doc: Document) => void
-) {
-  const results = Array<string>();
-  for (const saved of saves) {
-    const idx2 = (slide.txt as string[]).findIndex((x) =>
-      isEqual(x, saved.txt as string)
-    );
-    if (idx2 > -1) {
-      results.push(saved.result as string);
-    }
-  }
-  slide.setResults(results);
-  showSlides(doc);
-}
-
 export function showButton(doc: Document, txt: string): HTMLElement {
   const continue_btn = continueButton(doc);
   continue_btn?.addEventListener('click', (): void => {
