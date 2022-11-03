e33a465ed55ee5faccadea859dd49b6b97291092
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-11-03 02:40:49 -0400

    refactoring

--- src/ts/test/quiz/slide/slideType/vocab.test.ts
+++ src/ts/test/quiz/slide/slideType/vocab.test.ts
@@ -3,7 +3,7 @@ import type { Slide } from '../../../../main/quiz/slide';
 import { Vocab } from '../../../../main/quiz/slide/slideType/vocab';
 import { VOCAB } from '../../../../main/quiz/slideFactory';
 import { SlideTest } from '../../slide.test';
-class Test extends SlideTest<string[]> {
+class Test extends SlideTest {
   type = 'vocab';
   public processJson(): void {
     const param = VOCAB();
