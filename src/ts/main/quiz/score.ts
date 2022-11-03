cbf0f1d0a48da3c8c7d4c026e599424cb9f2fdce
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-11-03 19:56:38 -0400

    converting vocab to array of mc

--- src/ts/main/quiz/score.ts
+++ src/ts/main/quiz/score.ts
@@ -1,7 +1,6 @@
 import { isEqual } from '../utilities';
 import type { Course } from './course';
 import { percentCorrect } from './evaluate';
-// import { exerciseGroupScore2 } from './exerciseGroup';
 import { SaveData } from './slide/saveData';
 import { initSlide } from './slideFactory';
 import type { SlideInterface } from './slideInterface';
@@ -86,7 +85,6 @@ export class Score {
   ) {
     const slides = initSlide(exercise);
     const isArray = Array.isArray(slides);
-    //TODO: factor out code in common with MakeSlides.showSlides() and Slide.getSlideSavedIndex()
     if (isArray) {
       slides.forEach((slide) => {
         const idx = saves.findIndex((x) => isEqual(x.txt, slide.txt as string));
