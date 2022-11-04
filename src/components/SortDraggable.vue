2772968781281def768ec50aa1775ad16cc431ea
Sasha <chan.sasha16@gmail.com>
2022-11-04 16:51:04 +0700

    added sort component

---
+++ src/components/SortDraggable.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="sortContainer">
+    <p>sort</p>     
+
+    <draggable
+      :list="list"
+      :disabled="!enabled"
+      item-key="name"
+      class="list-group"
+      ghost-class="ghost"
+      :animation="100"
+    >
+      <template #item="{ element }">
+        <li>
+          <div class="list-group-item">
+            {{ element.name }}
+          </div>
+        </li>
+      </template>
+    </draggable>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import draggable from 'vuedraggable'
+
+const enabled = ref(true)
+const list = ref([
+  { name: 'a', id: 0 },
+  { name: 'b', id: 1 },
+  { name: 'c', id: 2 },
+  { name: 'd', id: 2 }
+])
+</script>
+
+<style>
+.list-group-item, .ghost {
+  justify-content: center;
+  align-items: center;
+  display: inline-flex;
+  background: #141E30;
+  border: solid 2px white;
+
+}
+.ghost > div {
+  visibility:hidden;
+}
+.sortContainer {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.list-group {
+  width: 500px;
+  display: inline-block;
+}
+.list-group-item {  
+  cursor: move;
+  height: 90px;
+  width: 500px;
+  margin: 8px;
+  color: white;
+}
+li {
+  list-style: none;
+}
+</style>
