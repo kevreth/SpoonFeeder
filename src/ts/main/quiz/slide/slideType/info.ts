6c9749763bbb42d850806be5ac1c167241f6aaba
Kevin Rethwisch <rethwisch_kevin@va.gov>
2022-11-19 17:23:33 -0500

    refactoring

--- src/ts/main/quiz/slide/slideType/info.ts
+++ src/ts/main/quiz/slide/slideType/info.ts
@@ -1,7 +1,13 @@
 import { Slide } from '../../slide';
+import { SlideInterface } from '../../slideInterface';
 import type { CreateHtmlTypeInfo } from '../strategies/createHtmlStrategy';
 import type { MakeSlidesTypeInfo } from '../strategies/makeSlidesStrategy';
 export class Info extends Slide {
+  decorate(setValues: SlideInterface, doc: Document): boolean {
+    setValues.getAns();
+    doc.getRootNode();
+    throw new Error('Method not implemented.');
+  }
   processJson(json: Info): void {
     ({ txt: this.txt } = json);
   }
