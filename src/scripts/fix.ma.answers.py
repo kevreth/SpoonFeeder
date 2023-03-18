c3572165a2d75778443048d3b1db95f72196e779
kev <kevinrethwisch@gmail.com>
2023-03-19 01:36:39 +0800

    fixing aws-review course file

--- src/scripts/fix.ma.answers.py
+++ src/scripts/fix.ma.answers.py
@@ -1,27 +1,40 @@
 import yaml
 from yaml.loader import SafeLoader
-with open('../courses/aws-review/course.yml') as f:
-    questions = yaml.load(f, Loader=SafeLoader)
-for part in questions:
-    for exercise in part['exercises']:
-        ans = exercise['ans']
-        arr = ans.split(',')
-        positions = []
-        for item in arr:
-            pos = ord(item) - 65
-            positions.append(pos)
-        options = exercise['o']
-        for pos in positions:
+with open('course1.yml', encoding="utf8") as f:
+  course = yaml.load(f, Loader=SafeLoader)
+units = course['units']
+ctr = 0
+for unit in units:
+  lessons = unit['lessons']
+  for lesson in lessons:
+    modules = lesson['modules']
+    for module in modules:
+      exercises = module['exercises']
+      for exercise in exercises:
+        if 'numans' in exercise:
+          numans = exercise['numans']
+          numans = str(numans)
+          positions = numans.split(',')
+          options = exercise['o']
+          # print (options)
+          for pos in positions:
+            pos = int(pos)
+            pos -= 1
             if pos==0: #no need to move anything if correct answer is in zero position.
-                continue
+              continue
             item = options.pop(pos)
             options.insert(0, item)
             exercise['o'] = options
-        length = len(arr)
-        print(exercise['type'], end ="")
-        if length == 1:
-            del exercise['ans']
+          # print (options)
+          length = len(positions)
+          ctr+=1
+          # print(ctr, exercise['type'], numans, length)
+          if length == 1:
+            if 'numans' in exercise:
+              del exercise['numans']
             exercise['type'] = 'mc'
-        else:
-            exercise['ans'] = length
+          else:
+            if 'numans' in exercise:
+              exercise['numans'] = length
             exercise['type'] = 'ma'
+print(yaml.dump(course, sort_keys=False, width=float("inf")))
