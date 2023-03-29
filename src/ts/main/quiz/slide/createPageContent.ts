b2b222328d243a84a6c5ab9fa24dd4d20a1b662b
kev <kevinrethwisch@gmail.com>
2023-03-30 03:28:45 +0800

    refactoring

--- src/ts/main/quiz/slide/createPageContent.ts
+++ src/ts/main/quiz/slide/createPageContent.ts
@@ -3,9 +3,7 @@ import { postRender } from './postRender';
 //small file but broken out to solve circular dependencies
 export function createPageContent(html: string, doc: Document): void {
   const element = doc.getElementById('continueBtn') as HTMLElement | null;
-  if (element != null) {
-    element.remove(); // Removes the div with the 'div-02' id
-  }
+  if (element != null) element.remove(); // Removes the div with the 'div-02' id
   empty('#content');
   append('#content', html);
   postRender(document);
