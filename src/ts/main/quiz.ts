9dc31dcabc3b7a8edad959d3eb6d6a96155b7bbb
kev <kevinrethwisch@gmail.com>
2023-03-30 14:23:27 +0800

    refactoring

--- src/ts/main/quiz.ts
+++ src/ts/main/quiz.ts
@@ -1,4 +1,4 @@
-import { CourseFile, Json } from './quiz/datalayer/globals';
+import { Json } from './quiz/datalayer/globals';
 import type { Course } from './quiz/datalayer/course';
 import { MakeSlides } from './quiz/makeSlides';
 import { ProcessJson } from './quiz/datalayer/processJson';
@@ -14,7 +14,6 @@ export class Quiz {
     const yamlFilename = Quiz.makeYamlFilename(courseName);
     //test data
     getYaml(yamlFilename, (course: Course) => {
-      CourseFile.set(course);
       const slides = processJson(course);
       Json.set(slides);
       MakeSlides.showSlides(doc);
--- src/ts/main/quiz/datalayer/globals.ts
+++ src/ts/main/quiz/datalayer/globals.ts
@@ -1,15 +1,4 @@
-import type { Course } from './course';
 import type { SlideInterface } from '../slideInterface';
-//Should be replaced by a Pinia store
-export class CourseFile {
-  private static json: Course;
-  public static get() {
-    return this.json;
-  }
-  public static set(json: Course) {
-    this.json = json;
-  }
-}
 export class Json {
   private static counter = 0;
   private static json: Array<SlideInterface> = [];
