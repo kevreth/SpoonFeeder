937363ca615197b72a9f3dbb1d5385e012eeb207
kevreth <kevinrethwisch@gmail.com>
2023-03-31 23:25:12 -0400

    refactoring

--- src/ts/main/quiz/slide/strategies/makeSlides/makeSlidesStrategyInfo.ts
+++ src/ts/main/quiz/slide/strategies/makeSlides/makeSlidesStrategyInfo.ts
@@ -1,7 +1,6 @@
-import { createContinueButton, MakeSlides } from '../../../makeSlides';
 import type { SlideInterface } from '../../../slideInterface';
+import { conclude } from '../../conclude';
 import { createPageContent } from '../../createPageContent';
-import { SaveData } from '../../../datalayer/saveData';
 import type { CreateHtmlTypeInfo } from '../createHtmlStrategy';
 export function makeSlidesStrategyInfo(
   txt: string,
@@ -11,24 +10,7 @@ export function makeSlidesStrategyInfo(
 ) {
   const html = createHtml(txt);
   createPageContent(html, doc);
-  conclude(slide, doc, txt);
+  slide.cont = true;
+  conclude(doc, slide, '', txt);
 }
-function conclude(slide: SlideInterface, doc: Document, txt: string) {
-  slide.saveData();
-  showButton(doc, txt);
-}
-//info has it's own showButton because the continue button
-//appears immediately upon page load and can be in the wrong
-//place because dynamic content in course files may cause it
-//to overlap. It gets placed before the dynamic content loads.
-//The continue button is placed inside #content here, unlike
-//other slide types where doing so causes the content to re-center
-//vertically.
-function showButton(doc: Document, txt: string): void {
-  const container = 'content';
-  const continue_btn = createContinueButton(doc, container, '<br>');
-  continue_btn.addEventListener('click', (): void => {
-    SaveData.setContinueTrue(txt);
-    MakeSlides.showSlides(doc);
-  });
-}
+
