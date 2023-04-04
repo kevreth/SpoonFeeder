989e52cd84233dde44ffd89a2bd49b6508b1e824
kevreth <kevinrethwisch@gmail.com>
2023-04-04 06:14:39 -0400

    init

--- src/ts/main/quiz/slide/explanation.ts
+++ src/ts/main/quiz/slide/explanation.ts
@@ -5,28 +5,31 @@ export function explanation(slide: SlideInterface): string {
   const distrators = [];
   const exp = slide.exp as string;
   const ref = slide.ref as string;
-  const answer = pluralize('Answer', answers.length);
-  const distrator = pluralize('Distractor', distrators.length);
-  const titleExplantion = title(exp, 'Explanation');
-  const titleReference = title(ref, 'Reference');
+  const answer = removeLinebreaks(pluralize('<b>Answer</b>', answers.length));
+  const distrator = removeLinebreaks(pluralize('Distractor', distrators.length));
+  const titleExplanation = title(exp, '<b>Explanation</b>');
+  const titleReference = title(ref, '<br>Reference(s)');
   const titleDistractor = title(distrator, 'Distractor');
   const titleResponse = title(
     slide.res === undefined ? '' : slide.res.toString(),
-    'Response'
+    '<b>Response</b>'
   );
   const explanation = `
   <center><b>${slide.txt}</b></center> <br>
-    <b>${answer}</b> <br>
+    ${answer}
     ${(slide.ans as string).toString()} <br>
-    ${titleDistractor}  <br>
-    <b>${titleResponse}</b> <br>
-    ${(slide.res as string).toString()} <br><br>
-    <b>${titleExplantion}</b>
-    ${exp === undefined ? '' : exp}
-    ${titleReference}(s):
-    ${ref}
+    ${titleDistractor}
+    ${titleResponse}
+    ${removeLinebreaks((slide.res as string).toString())} <br>
+    ${titleExplanation}
+    ${exp === undefined ? '' : removeLinebreaks(exp)} <br>
+    ${titleReference}
+    ${removeLinebreaks(ref)}
   `;
   return explanation;
+}
+function removeLinebreaks(str: string): string {
+  return str.replaceAll('<br>', '');
 }
 function pluralize(str: string, length: number): string {
   length >= 1 ? '== ' + (str += 's') : (str = '');
@@ -34,6 +37,6 @@ function pluralize(str: string, length: number): string {
 }
 function title(ref: string, label: string) {
   let retval = '';
-  if (ref !== undefined && ref.length > 0) retval = label;
+  if (ref !== undefined && ref.length > 0) retval = label + ' <br>';
   return retval;
 }
