89062b22af624003834d2639c4ba97935ee759d9
Sasha <chan.sasha16@gmail.com>
2023-04-08 20:07:40 +0700

    set up the logic for selecting course

--- src/components/CourseSelector.vue
+++ src/components/CourseSelector.vue
@@ -1,29 +1,53 @@
 <template>
   <transition appear group
-  enter-active-class="animated zoomInUp"
-  leave-active-class="animated zoomOutDown"
-  >
-  <q-overlay id="courseTable" @click.stop="">
-    <template #body>
-      <q-list id="courseList" class="smaller-font fixed-center bg-secondary courseList">
-        <q-item :header="true" class="titleCourse" id="titleCourse">
-          <q-item-label header class="headerCourse">Course</q-item-label>
-        </q-item>
-        <div class="scrollable-course" id="courses">
-          <q-item clickable v-for="course in courses" :key="course" id="wrapCourses">
-            <q-item-section class="course" id="course">{{ course }}</q-item-section>
+    enter-active-class="animated zoomInUp"
+    leave-active-class="animated zoomOutDown">
+    <q-overlay
+      id="courseTable"
+      @click.stop="">
+      <template #body>
+        <q-list
+          id="courseList"
+          class="smaller-font fixed-center bg-secondary courseList">
+          <q-item
+            :header="true"
+            class="titleCourse"
+            id="titleCourse"
+          >
+            <q-item-label header class="headerCourse">Course</q-item-label>
           </q-item>
-        </div>
-        <div class="btnCourse">
-          <SwitchCourse></SwitchCourse>
-          <ExitBtn
-              @click="closeInfo"
-              color="primary"/>
-        </div>
-      </q-list>
-    </template>
-  </q-overlay>
-</transition>
+          <div class="scrollable-course" id="courses">
+            <q-item
+              clickable
+              v-for="course in courses"
+              :key="course"
+              id="wrapCourses"
+              @click="selectedCourse = course"
+              :class="{ 'selected': course === selectedCourse }"
+            >
+              <q-item-section
+                class="course"
+                id="course"                
+              >
+                {{ course }}
+              </q-item-section>
+            </q-item>
+          </div>
+          <div class="btnCourse">
+            <SwitchCourse
+              :selectCourse="selectCourse"
+              :selectedCourse="selectedCourse"
+              @closeInfo="closeInfo"
+            />
+            <ExitBtn
+                @click="closeInfo"
+                color="primary"
+            />
+          </div>
+        </q-list>
+      </template>
+    </q-overlay>
+  </transition>
 </template>
 
 <script setup>
@@ -33,6 +57,16 @@ import { getCourseListing } from '../ts/main/utilities';
 import SwitchCourse from './SwitchCourse.vue';
 
 const courses = ref(getCourseListing());
+const selectedCourse = ref(null);
+
+function selectCourse(course) {
+  selectedCourse.value = course
+  console.log('selected course', selectedCourse)
+}
+
+// function closeTable() {
+//   selectedCourse.value = null;
+// }
 
 const emit = defineEmits(['closeInfo'])
 function closeInfo() {
@@ -41,6 +75,10 @@ function closeInfo() {
 </script>
 
 <style>
+.selected {
+  background: #3c3b41;
+  color: #f2c037;
+}
 .titleCourse {
   padding: 0;
   position: sticky;
--- src/components/SwitchCourse.vue
+++ src/components/SwitchCourse.vue
@@ -1,6 +1,36 @@
 <template>
   <q-btn
-    label="Switch"
+    v-if="courseSelected"
     class="switchBtn q-mb-sm q-mr-sm q-ml-xs bg-primary"
+    label="switch"
+    @click="switchCourse()"
   />
 </template>
+
+<script setup>
+  import { ref, watchEffect } from 'vue';
+
+  const props = defineProps({
+    selectCourse: {
+      type: Function,
+      required: true
+    },
+    selectedCourse: {
+      required: true
+    }
+  })
+  const emit = defineEmits(['closeInfo']);
+
+  const courseSelected = ref(false);
+
+  function switchCourse() {
+    props.selectCourse(props.selectedCourse);
+    emit('closeInfo');
+  }
+
+  watchEffect(() => {
+    if(props.selectedCourse) {
+      courseSelected.value = true;
+    }
+  })
+</script>
