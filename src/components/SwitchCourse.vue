8fc6e3d5c0ac36e38dfef0092eee448b128351ae
dropdown disappear after selected <chan.sasha16@gmail.com>
2023-04-10 17:35:39 +0700

    disable switch when course list didn't updated

--- src/components/SwitchCourse.vue
+++ src/components/SwitchCourse.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+
   const props = defineProps({
     selectCourse: {
       type: Function,
@@ -17,9 +19,13 @@
     }
   })
   const emit = defineEmits(['closeInfo']);
+  const prevSelectedCourse = ref(null);
+  onMounted(() => {
+    prevSelectedCourse.value = props.selectedCourse
+  })
 
   function switchCourse() {
-    if (props.selectedCourse) {
+    if (props.selectedCourse && props.selectedCourse !== prevSelectedCourse.value ) {
       props.selectCourse(props.selectedCourse);
       emit('closeInfo');
     }
