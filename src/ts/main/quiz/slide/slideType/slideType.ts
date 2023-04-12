f04ceac8832c5a675866910b476412b89102bc67
kevreth <kevinrethwisch@gmail.com>
2023-04-12 02:38:16 -0400

    Slidetype

--- src/ts/main/quiz/slide/slideType/slideType.ts
+++ src/ts/main/quiz/slide/slideType/slideType.ts
@@ -1,6 +1,9 @@
+import { AdocVisitorInterface } from '../../datalayer/adocVisitor';
 import { SlideInterface } from '../../slideInterface';
 
 export interface SlideType {
   processJson(json: SlideInterface): void;
+  accept(visitor: AdocVisitorInterface): void;
+  makeSlides(doc: Document): void
   decorate(doc: Document): boolean;
 }
