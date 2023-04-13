8e6068e799f3df6321213dc4a65835058faf6287
kevreth <kevinrethwisch@gmail.com>
2023-04-13 09:45:08 -0400

    refactor

---
+++ src/ts/main/quiz/quiz/buttons.ts
@@ -0,0 +1,59 @@
+import reloadPage from '../../../../vue/composables/startOver';
+import { SaveData } from '../datalayer/mediator';
+import { showSlides } from '../slide/slideDispatcher';
+import type { SlideInterface } from '../slide/slideInterface';
+export function makeButton(id: string, clazz: string, content: string): string {
+  return `<button id="${id}" class="${clazz}" type="button">${content}</button>`;
+}
+export function doneButton() {
+  return makeButton('btn', 'done', 'done');
+}
+export function showButton(doc: Document, txt: string): HTMLElement {
+  const continue_btn = continueButton(doc);
+  continue_btn?.addEventListener('click', (): void => {
+    SaveData.setContinueTrue(txt);
+      const explain = doc.getElementById('explainIcon') as HTMLElement;
+      explain.style.visibility = 'hidden';
+    showSlides(doc);
+  });
+  return continue_btn;
+}
+export function continueButton(doc: Document): HTMLElement {
+  const container = 'slide';
+  const continue_btn = createContinueButton(doc, container, '');
+  //as an HTMLElement we can assign styles
+  //this wouldn't work when using a stylesheet, not sure why.
+  continue_btn.style.position = 'absolute';
+  continue_btn.style.marginTop = 10 + 'px';
+  continue_btn.style.marginLeft = -2.3 + 'em';
+  return continue_btn;
+}
+export function createContinueButton(
+  doc: Document,
+  container: string,
+  prefix: string
+) {
+  const button = makeButton('continueBtn', 'continueBtn', 'continue');
+  const slide = doc.getElementById(container) as HTMLElement;
+  slide.insertAdjacentHTML('beforeend', prefix + button);
+  const continue_btn = doc.getElementById('continueBtn') as HTMLElement;
+  return continue_btn;
+}
+export function startOverButton(doc: Document) {
+  const startOverText = makeButton('startOver', 'startOver', 'Start Over');
+  doc.body.insertAdjacentHTML('beforeend', '<br>' + startOverText);
+  const startOver = document.getElementById('startOver') as HTMLElement;
+  startOver.addEventListener('click', () => {
+    reloadPage();
+  });
+}
+export function showExplainIcon(slide: SlideInterface, doc: Document) {
+  if (slide.exp !== undefined && slide.exp !== '') {
+    const explain = doc.getElementById('explainIcon') as HTMLElement;
+    explain.style.visibility = 'visible';
+  }
+}
+export function hideExplainIcon(doc: Document) {
+  const explain = doc.getElementById('explainIcon') as HTMLElement;
+  explain.style.visibility = 'hidden';
+}
---
+++ src/ts/main/quiz/quiz/evaluate.ts
@@ -0,0 +1,76 @@
+export const ROW = '<tr><td>%Q%</td><td>%N%.</td><td>%A%</td><td>%C%</td></tr>';
+import type { AnswerType } from '../slide/strategies/resultStrategy';
+import type { SlideInterface } from '../slide/slideInterface';
+export class Evaluation {
+  constructor(
+    public responses: number,
+    public correct: number,
+    public text: string
+  ) {}
+}
+export function summary(
+  responseCtr: number,
+  correctCtr: number,
+  pctCorrect: string
+) {
+  return `NUMBER OF QUESTIONS: ${responseCtr}<br>\nNUMBER CORRECT: ${correctCtr}<br>\nPERCENT CORRECT: ${pctCorrect}%`;
+}
+export function percentCorrect(
+  correctCtr: number,
+  responseCtr: number
+): string {
+  if (responseCtr === 0) return '0'; // prevent divide-by-zero error
+  return ((correctCtr / responseCtr) * 100).toFixed(0);
+}
+export function makeRow(
+  question: string,
+  response: AnswerType,
+  answer: AnswerType
+) {
+  let text = ROW;
+  text = text.replace('%Q%', question);
+  text = text.replace('%A%', response as string);
+  text = text.replace('%C%', answer as string);
+  return text;
+}
+export function numbering(responseCtr: number, text: string) {
+  for (let i = 1; i < responseCtr + 1; i++) {
+    text = text.replace('%N%', i.toString());
+  }
+  return text;
+}
+export function evaluate(slidesArr: SlideInterface[]): string {
+  const evals = getEvaluationArray(slidesArr);
+  const TABLE_HEADER =
+    '<table><tr><th>Question</th><th></th><th>Your answer</th><th>Correct Answer</th></tr>';
+  const evalAccum = new Evaluation(0, 0, TABLE_HEADER);
+  for (const evaluation of evals) {
+    evalAccum.responses += evaluation.responses;
+    evalAccum.correct += evaluation.correct;
+    evalAccum.text = evalAccum.text.concat(evaluation.text);
+  }
+  evalAccum.text = numbering(evalAccum.responses, evalAccum.text);
+  evalAccum.text = evalAccum.text.concat('</table>');
+  const txt0 = evalAccum.text;
+  const correct = evalAccum.correct;
+  const responses = evalAccum.responses;
+  const txt1 = evalStats(correct, responses);
+  return txt0.concat(txt1);
+}
+export function getEvaluationArray(slidesArr: SlideInterface[]) {
+  const size = slidesArr.length;
+  const evals = new Array<Evaluation>();
+  for (let i = 0; i < size; i++) {
+    const slide = slidesArr[i];
+    if (slide && slide.isExercise) {
+      const evaluation: Evaluation = slide.evaluate();
+      evals.push(evaluation);
+    }
+  }
+  return evals;
+}
+export function evalStats(correct: number, responses: number) {
+  const pctCorrect = percentCorrect(correct, responses);
+  const txt = summary(responses, correct, pctCorrect);
+  return txt;
+}
