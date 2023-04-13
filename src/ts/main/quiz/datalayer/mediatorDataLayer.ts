67bae1ec2a6f6fbe0762475a81a404c64bd7bbd9
kevreth <kevinrethwisch@gmail.com>
2023-04-13 08:17:31 -0400

    refactor

---
+++ src/ts/main/quiz/datalayer/mediatorDataLayer.ts
@@ -0,0 +1,22 @@
+import type { AdocVisitorInterface } from './courseData/adocVisitor';
+import type { Course, Division } from './courseData/course';
+import { ProcessJson } from './courseData/processJson';
+import { CourseFile, Json } from './globals';
+import { PREFIX_COURSE_FILE, loadCourse } from './courseData/loadCourse';
+import type { DivisionProcessor } from './courseData/courseFileProcessor';
+import { process } from './courseData/courseFileProcessor';
+import { SaveData } from './saveData/saveData';
+
+export {
+  Course,
+  Division,
+  AdocVisitorInterface,
+  ProcessJson,
+  CourseFile,
+  Json,
+  PREFIX_COURSE_FILE,
+  loadCourse,
+  process,
+  SaveData,
+  DivisionProcessor
+}
---
+++ src/ts/main/quiz/slide/markupColors.ts
@@ -0,0 +1,10 @@
+export enum MarkupColors {
+  INDETERMINANT = 'blue',
+  CORRECT = 'green',
+  INCORRECT = 'red',
+}
+export const INDETERMINANT = MarkupColors.INDETERMINANT;
+export const CORRECT = MarkupColors.CORRECT;
+export const INCORRECT = MarkupColors.INCORRECT;
+//USAGE
+//import {INDETERMINANT, CORRECT, INCORRECT} from '../../../MarkupColors';
