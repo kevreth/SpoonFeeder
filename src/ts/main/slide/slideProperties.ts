28f37b2713ef1a6584f476d9c953156dc0f4fa64
kevreth <kevinrethwisch@gmail.com>
2023-04-14 21:47:54 -0400

    refactor

--- src/ts/main/slide/slideProperties.ts
+++ src/ts/main/slide/slideProperties.ts
@@ -31,11 +31,11 @@ export type SlidePropertiesSort = SlidePropertiesResponsive;
 export interface SlidePropertiesVocab extends SlidePropertiesResponsive {
   list: Map<string, string>;
 }
-export type SlidePropertiesType = SlidePropertiesGap &
-  SlidePropertiesImap &
-  SlidePropertiesInfo &
-  SlidePropertiesMc &
-  SlidePropertiesMa &
-  SlidePropertiesSelect &
-  SlidePropertiesSort &
+export type SlidePropertiesType = SlidePropertiesGap |
+  SlidePropertiesImap |
+  SlidePropertiesInfo |
+  SlidePropertiesMc |
+  SlidePropertiesMa |
+  SlidePropertiesSelect |
+  SlidePropertiesSort |
   SlidePropertiesVocab;
