cb4f1dc4e7ace4ac91115059e2467dab39191535
kevreth <kevinrethwisch@gmail.com>
2023-04-20 07:13:25 -0400

    di

--- src/ts/main/slide/conclude/conclude.ts
+++ src/ts/main/slide/conclude/conclude.ts
@@ -3,20 +3,30 @@ import type { AnswerType } from '../../slidetype/mediator';
 import type { SlideInterface } from '../slideInterface';
 import { playAudio } from './audio';
 
-export function conclude2(
+export function conclude(
   doc: Document,
   slide: SlideInterface,
   res: AnswerType,
-  txt: string
+  txt: string,
+  _playAudio: ( isCorrect: boolean ) => void,
+  _showExplainIcon: (slide: SlideInterface, doc: Document) => void,
+  _showButton: (doc: Document, txt: string) => void,
 ) {
   slide.setRes(res);
   if (!slide.immediateConclusion) {
     const isCorrect = slide.decorate(doc);
-    playAudio(isCorrect);
+    _playAudio(isCorrect);
   }
   slide.saveData();
   const done = doc.getElementById('btn');
   if (done !== null) done.remove();
-  showExplainIcon(slide, doc);
-  showButton(doc, txt);
+  _showExplainIcon(slide, doc);
+  _showButton(doc, txt);
+}
+export function conclude2(
+  doc: Document,
+  slide: SlideInterface,
+  res: AnswerType,
+  txt: string) {
+  conclude( doc, slide, res, txt, playAudio, showExplainIcon, showButton );
 }
