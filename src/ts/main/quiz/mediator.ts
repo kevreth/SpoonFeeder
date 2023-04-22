e678954b591d621ca659548658140d6a5bbb167a
kevreth <kevinrethwisch@gmail.com>
2023-04-22 00:46:22 -0400

    refactor

--- src/ts/main/quiz/mediator.ts
+++ src/ts/main/quiz/mediator.ts
@@ -8,32 +8,23 @@ export {
   SaveData,
   adoc2html,
   clearSessionStorage,
-  loadCourse,
-  process,
+  loadCourse
 } from '../course/mediator';
-export type { Course, Division, DivisionProcessor } from '../course/mediator';
+export type { Division } from '../course/mediator';
 export { Score } from '../course/score/score';
 export { ScoreProcessor } from '../course/score/scoreProcessor';
 export { SummaryLine } from '../course/score/summaryLine';
 export type { ISummaryLine } from '../course/score/summaryLine';
-export {
-  difference,
-  intersection,
-  isEqual,
-  last,
-  marked,
-  shuffle,
-} from '../mediator';
+export { difference, intersection, marked, shuffle } from '../mediator';
 export { explanation } from '../slide/explanation';
 export type { AnswerType, SlideInterface } from '../slide/mediator';
 export { INFO, initSlide } from '../slidetype/mediator';
-export { AdocVisitor } from '../slidetype/misc/adocVisitor';
-export type { AdocVisitorInterface } from '../slidetype/misc/adocVisitor';
+export type { } from '../slidetype/misc/adocVisitor';
 export {
   continueButton,
   doneButton,
   makeButton,
-  startOverButton,
+  startOverButton
 } from './buttons';
 export { Evaluation, evaluate, makeRow, percentCorrect } from './evaluate';
 export { hideExplainIcon, showExplainIcon } from './explainIcon';
@@ -42,3 +33,4 @@ export { fillMatchingSlide, showSlides } from './slideDispatcher';
 export { dispatch2 } from './stateActionDispatcher';
 export type { StateActions } from './stateActionDispatcher';
 export { getChildIds, remove, removeListener } from './utilities';
+
