2eba0a94b56322a34ce740a563f674a6ff6b3307
sasha <chan.sasha16@gmail.com>
2023-04-23 15:02:37 +0700

    fixed mute issue

--- src/ts/main/slide/conclude/audio.ts
+++ src/ts/main/slide/conclude/audio.ts
@@ -1,18 +1,18 @@
-import { WebStorageFlag } from '../../dataaccess/mediator';
+import { MUTE, WebStorageFlag } from '../../dataaccess/mediator';
 
 export class AudioPlayer {
   private readonly INCORRECT: string = '/resources/audio/incorrect.mp3';
   private readonly CORRECT: string = this.INCORRECT.replace('in', '');
-  constructor(public audio: HTMLAudioElement, public mute: WebStorageFlag) {}
+  constructor(public audio: HTMLAudioElement, public MUTE: WebStorageFlag) {}
   public playAudio(correct: boolean): void {
-    if (this.mute.is()) return;
+    if (MUTE.is()) return;
     this.audio.src = this.INCORRECT;
     if (correct) this.audio.src = this.CORRECT;
     this.audio.play();
   }
-  public muteAudio(): void {
+  public muteAudio() {
     this.audio.muted = true;
-    this.mute.clear();
+    MUTE.set();
   }
   public playBack(): void {
     this.audio.muted = false;
