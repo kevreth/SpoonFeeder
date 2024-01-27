05b9c94c3c90e168a0142315851b8d8ca1b22b33
Kevin Rethwisch <kevinrethwisch@gmail.com>
2024-01-27 01:52:18 -0500

    docker

---
+++ src/courses/docker/slides.py
@@ -0,0 +1,88 @@
+import sys
+import yaml
+from bs4 import BeautifulSoup
+
+def read_yaml(file_path):
+  with open(file_path, 'r') as file:
+    return yaml.safe_load(file)
+
+def verify_yaml(yaml_content):
+  return isinstance(yaml_content, dict)
+
+def passthrough(str):
+  return f'\n\n++++\n{str}\n++++\n\n'
+
+def generate_html_body(yaml_content):
+  html_body = ''
+  for lesson in yaml_content.get('lessons', []):
+    html_body += passthrough(f"<h2>{lesson['name']}</h2>")
+    for module in lesson.get('modules', []):
+      html_body += passthrough(f"<h3>{module['name']}</h3>")
+      insts = module.get('inst')
+      if insts is not None:
+        for inst in insts:
+          if inst:
+            html = extract_inner_content(inst)
+            html_body += html + passthrough('<hr>')
+  return html_body
+
+def extract_inner_content(inst):
+  content_adoc = inst.get('txt')
+  sdbr_adoc = inst.get('sdbr')
+  retval = ''
+  if sdbr_adoc:
+    retval = f'''
+++++
+<div class="two-columns">
+  <div class="sidebar">
+++++
+
+{sdbr_adoc}
+
+++++
+  </div>
+<div class="content">
+++++
+
+{content_adoc}
+
+++++
+  </div>
+</div>
+++++
+    '''
+  else: retval = content_adoc
+  return retval
+
+def substitute_html_body(template_file, html_body):
+  with open(template_file, 'r') as file:
+    soup = BeautifulSoup(file, 'html.parser')
+    soup.body.clear()
+    soup.body.append(BeautifulSoup(html_body, 'html.parser'))
+  return str(soup)
+
+def main():
+  if len(sys.argv) < 2:
+    print("Usage: python script.py <YAML file path>")
+    sys.exit(1)
+
+  yaml_file = sys.argv[1]
+  template_file = 'template.html'
+
+  yaml_content = read_yaml(yaml_file)
+  if not verify_yaml(yaml_content):
+    print("Invalid YAML file.")
+    sys.exit(1)
+
+  html_body = generate_html_body(yaml_content)
+  html_body = passthrough(html_body)
+  final_html = substitute_html_body(template_file, html_body)
+  final_html = passthrough(final_html)
+
+  output_file = yaml_file.rsplit('.', 1)[0] + ".adoc"
+  with open(output_file, 'w') as file:
+    file.write(final_html)
+  print(f"ADOC file created: {output_file}")
+
+if __name__ == "__main__":
+  main()
