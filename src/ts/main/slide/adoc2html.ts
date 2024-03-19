584838f659ba9eaeb135dba6ac097b3a29c1c475
Kevin Rethwisch <kevinrethwisch@gmail.com>
2024-03-19 07:07:47 -0400

    fixed.image.bug

--- src/ts/main/slide/adoc2html.ts
+++ src/ts/main/slide/adoc2html.ts
@@ -10,9 +10,18 @@ import { COURSE_NAME, marked } from '../dataaccess/mediator';
 // external files (svg and html).
 export function adoc2html(str: string): string {
   if (typeof str === 'undefined') return '';
+  const course = COURSE_NAME.get();
+  // console.log(str)
+  // let txt = str.replace('image::',`image::src/courses/${course}/`);
+  // console.log(txt)  
   let txt = adoc2markdown(str as string);
+  console.log(txt) 
   txt = markdown2html(txt);
   //consider adding a beautification step
+  console.log(txt)
+  txt = txt.replace('img src=\"',`img height = 300 
+  src="src/courses/${course}/`);
+  console.log(txt)  
   txt = insertHandlebars(txt);
   txt = processHandlebars(txt);
   return txt;
