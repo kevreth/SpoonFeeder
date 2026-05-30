dfce33b0a1716691bf13101f4a86ce0790612ac8
Kevin Rethwisch <kevinrethwisch@gmail.com>
2026-05-30 15:02:50 +0800

    upgrade

--- src/vue/router/routes.ts
+++ src/vue/router/routes.ts
@@ -1,10 +1,12 @@
 import type { RouteRecordRaw } from 'vue-router';
+import MainLayout from '../layouts/MainLayout.vue';
+import IndexPage from '../pages/IndexPage.vue';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('../layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('../pages/IndexPage.vue') }],
+    component: MainLayout,
+    children: [{ path: '', component: IndexPage }],
   },
   { path: '/:pathMatch(.*)*', redirect: '/' },
 ];
